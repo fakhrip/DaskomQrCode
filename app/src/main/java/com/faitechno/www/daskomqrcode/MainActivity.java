3be97959eecff801c99d9d3be2301428427c7d63
fakhrip <fakhriputra123s@gmail.com>
2019-01-20 23:14:57 +0700

    second commit

--- app/src/main/java/com/faitechno/www/daskomqrcode/MainActivity.java
+++ app/src/main/java/com/faitechno/www/daskomqrcode/MainActivity.java
@@ -1,13 +1,69 @@
 package com.faitechno.www.daskomqrcode;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.WHITE;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button scanButton;
+    ImageView barcodeImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanButton = findViewById(R.id.scanButton);
+        barcodeImage = findViewById(R.id.barcodeImage);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ScanActivity.class));
+            }
+        });
+
+        try {
+            Bitmap bitmap = encodeAsBitmap("tset");
+            barcodeImage.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, 200, 200, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, 200, 0, 0, w, h);
+        return bitmap;
     }
 }
---
+++ app/src/main/java/com/faitechno/www/daskomqrcode/ScanActivity.java
@@ -0,0 +1,42 @@
+package com.faitechno.www.daskomqrcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+
+        //TODO
+        //SEND API REQUEST BASED ON NIM GOT FROM RESULT using result.getText()
+
+        onBackPressed();
+    }
+}
